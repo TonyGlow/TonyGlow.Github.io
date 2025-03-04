3d38d69822fa95654e189c3fd90249d958c58d12
TonyGlow <TonyGlowable@outlook.com>
2025-03-03 19:33:00 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,18 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const fullscreenButton = document.getElementById('fullscreen-button');
-    const gameIframe = document.getElementById('game-iframe');
+    const fullscreenButtons = document.querySelectorAll('#fullscreen-button');
 
-    fullscreenButton.addEventListener('click', () => {
-        if (gameIframe.requestFullscreen) {
-            gameIframe.requestFullscreen();
-        } else if (gameIframe.mozRequestFullScreen) { // Firefox
-            gameIframe.mozRequestFullScreen();
-        } else if (gameIframe.webkitRequestFullscreen) { // Chrome, Safari and Opera
-            gameIframe.webkitRequestFullscreen();
-        } else if (gameIframe.msRequestFullscreen) { // IE/Edge
-            gameIframe.msRequestFullscreen();
-        }
+    fullscreenButtons.forEach(button => {
+        button.addEventListener('click', () => {
+            const gameIframe = button.previousElementSibling.querySelector('iframe');
+            if (gameIframe.requestFullscreen) {
+                gameIframe.requestFullscreen();
+            } else if (gameIframe.mozRequestFullScreen) { // Firefox
+                gameIframe.mozRequestFullScreen();
+            } else if (gameIframe.webkitRequestFullscreen) { // Chrome, Safari and Opera
+                gameIframe.webkitRequestFullscreen();
+            } else if (gameIframe.msRequestFullscreen) { // IE/Edge
+                gameIframe.msRequestFullscreen();
+            }
+        });
     });
 });
